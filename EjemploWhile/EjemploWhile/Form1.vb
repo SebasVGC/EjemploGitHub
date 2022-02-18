aaa6f2e834f6f720354a9558568b194d7eb88c61
SebasVGC <83033498+SebasVGC@users.noreply.github.com>
2022-02-18 17:43:01 -0600

    Carga de archivos

---
+++ EjemploWhile/EjemploWhile/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim MyValue As String = ""
+        Do
+            MyValue = InputBox("Ingrese nombre", "Saludo", "", 2)
+        Loop While (MyValue = "")
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim index As Integer = 0
+        Dim resultado As String = ""
+        While (index <= 25)
+            If Not (index Mod 2 = 0) Then
+                index += 1
+                Continue While
+            End If
+            If (index > 20) Then
+                Exit While
+            End If
+            resultado = resultado + index.ToString + ","
+            index += 1
+        End While
+        TextBox1.Text = resultado
+    End Sub
+End Class
---
+++ EjemploWhile/EjemploWhile/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("EjemploWhile")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("EjemploWhile")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("74766768-41f4-4846-9679-6b3748dec9ed")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
